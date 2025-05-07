83c0ae16f97be5e16a183a09b1480f2a5cd5c681
der-johanni <ivangorbanwork@gmail.com>
2025-05-07 10:44:17 +0300

    new token implemented

---
+++ include/new_tokens.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+typedef enum {
+    OPERATOR,
+    FUNCTION,
+    NUMBER,
+    LEFT_PARANTHESIS,
+    RIGHT_PARANTHESIS,
+    SEPARATOR
+} token_type;
+
+class Token;
+
+using token_ptr = std::shared_ptr<Token>;
+
+class Token {
+private:
+    const std::string data;
+    const token_type type;
+public:
+    explicit Token(const std::string &name, const token_type &type) noexcept;
+
+    const std::string &get_data() const noexcept;
+
+    const token_type get_type() const noexcept;
+};
